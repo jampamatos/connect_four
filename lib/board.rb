12356ecbb73ef90a68f8a53496009222723faafe
jampamatos <jp.coutm@gmail.com>
2023-02-03 11:28:16 -0300

    Finished #valid_placement?, #column_full? and #place_chip methods, all tests are go

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
 class Board
-  attr_reader :rows, :columns, :dimensions
+  attr_reader :row_cell, :column_cell
+  attr_accessor :dimensions
 
   def initialize(rows = 6, columns = 7)
-    @rows = rows
-    @columns = columns
-    @dimensions = Array.new(rows) { Array.new(columns, 0) }
+    @dimensions = Array.new(rows) { Array.new(columns) }
+  end
+
+  def valid_placement?(column)
+    column >= 0 && column < dimensions[0].size && !column_full?(column)
+  end
+
+  def column_full?(column)
+    dimensions.each do |row|
+      return false if row[column].nil?
+    end
+    true
+  end
+
+  def place_chip(player, column)
+    return raise 'Column is full' if column_full?(column)
+    return raise 'Placement outside of board boundaries' unless valid_placement?(column)
+
+    row = dimensions.size - 1
+    while row >= 0
+      if dimensions[row][column].nil?
+        dimensions[row][column] = player.color
+        return row
+      end
+      row -= 1
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,45 +6,98 @@ describe Board do
   describe '#initialize' do
     it 'creates a 6x7 game board' do
       board = Board.new
-      expect(board.rows).to eq(6)
-      expect(board.columns).to eq(7)
+      expect(board.dimensions.size).to eq(6)
+      expect(board.dimensions[0].size).to eq(7)
     end
 
     it 'creates a 7x6 game board' do
       board = Board.new(7, 6)
-      expect(board.rows).to eq(7)
-      expect(board.columns).to eq(6)
+      expect(board.dimensions.size).to eq(7)
+      expect(board.dimensions[0].size).to eq(6)
     end
 
     it 'creates an empty game board' do
       board = Board.new
-      expect(board.dimensions).to eq(Array.new(6) { Array.new(7, 0) })
+      expect(board.dimensions).to eq(Array.new(6) { Array.new(7) })
     end
 
     it 'creates a game board with a minimum of 4 rows' do
       board = described_class.new(4, 6)
-      expect(board.rows).to eq(4)
-      expect(board.columns).to eq(6)
+      expect(board.dimensions.size).to eq(4)
+      expect(board.dimensions[0].size).to eq(6)
     end
 
     it 'creates a game board with a minimum of 4 columns' do
       board = described_class.new(6, 4)
-      expect(board.rows).to eq(6)
-      expect(board.columns).to eq(4)
+      expect(board.dimensions.size).to eq(6)
+      expect(board.dimensions[0].size).to eq(4)
+    end
+  end
+
+  describe '#valid_placement' do
+    subject(:board) { described_class.new }
+
+    it 'returns false if column is out of bounds' do
+      expect(board.valid_placement?(8)).to eq(false)
+    end
+
+    it 'returns true if placement is valid' do
+      expect(board.valid_placement?(0)).to eq(true)
+    end
+  end
+
+  describe '#column_full?' do
+    subject(:board_chip) { described_class.new }
+    let(:player) { double('player', color: 'red') }
+
+    it 'returns false if the column is not full' do
+      expect(board_chip.column_full?(0)).to be(false)
+    end
+
+    it 'returns true if the column is full' do
+      6.times do
+        board_chip.place_chip(player, 0)
+      end
+      expect(board_chip.column_full?(0)).to be(true)
+    end
+
+    it 'returns false if the column is not full after some chips have been placed' do
+      3.times do
+        board_chip.place_chip(player, 0)
+      end
+      expect(board_chip.column_full?(0)).to be(false)
     end
   end
 
   describe '#place_chip' do
-    xit 'place chip at the lowest unoccupied cell in a column' do
+    subject(:board_chip) { described_class.new }
+    let(:player) { double('player', color: 'red') }
+
+    it 'place chip at the bottom cell in first column when entire column is unoccupied' do
+      board_chip.place_chip(player, 0)
+      expect(board_chip.dimensions[5][0]).to eq('red')
     end
 
-    xit 'raise an error if the column is full and cannot place chip' do
+    it 'place chip at the second cell in first column when first cell is occupied' do
+      board_chip.place_chip(player, 0)
+      board_chip.place_chip(player, 0)
+      expect(board_chip.dimensions[4][0]).to eq('red')
     end
 
-    xit 'correctly update the board after chip placement' do
+    it 'place chip at the third cell in third column when two first cells are occupied' do
+      board_chip.place_chip(player, 2)
+      board_chip.place_chip(player, 2)
+      board_chip.place_chip(player, 2)
+      expect(board_chip.dimensions[3][2]).to eq('red')
     end
 
-    xit 'prevent placement outside the boundaries of the game board.' do
+    it 'raises an error if the column is full and cannot place chip' do
+      board_chip.dimensions = Array.new(6) { Array.new(7, player.color) }
+      expect { board_chip.place_chip(player, 0) }.to raise_error('Column is full')
+    end
+    
+    it 'raises an error if the placement is outside of board boundaries' do
+      expect { board_chip.place_chip(player, 7) }.to raise_error('Placement outside of board boundaries')
     end
   end
 end
