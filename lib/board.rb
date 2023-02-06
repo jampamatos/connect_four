17e3ae4a0a6a416ebb6f9474281c7d3b4b1f3797
João Paulo <jp.coutm@gmail.com>
2023-02-06 20:53:23 -0300

    Finished #check_diagonal and #game_over, all tests are green. Will write new test cases next.

--- lib/board.rb
+++ lib/board.rb
@@ -3,23 +3,20 @@
 require 'pry-byebug'
 
 class Board
-  attr_reader :row_cell, :column_cell
-  attr_accessor :dimensions
+  attr_accessor :grid
 
   def initialize(rows = 6, columns = 7)
-    @dimensions = Array.new(rows) { Array.new(columns) }
+    @grid = Array.new(rows) { Array.new(columns) }
   end
-
-
 
   def place_chip(player, column)
     return raise 'Column is full' if column_full?(column)
     return raise 'Placement outside of board boundaries' unless valid_placement?(column)
 
-    row = dimensions.size - 1
+    row = grid.size - 1
     while row >= 0
-      if dimensions[row][column].nil?
-        dimensions[row][column] = player.color
+      if grid[row][column].nil?
+        grid[row][column] = player.color
         return row
       end
       row -= 1
@@ -27,7 +24,7 @@ class Board
   end
 
   def check_horizontal
-    dimensions.each do |row|
+    grid.each do |row|
       next if row.compact.empty?
 
       winner = four_consecutive_chips?(row, 4)
@@ -37,7 +34,7 @@ class Board
   end
 
   def check_vertical
-    dimensions.transpose.each do |column|
+    grid.transpose.each do |column|
       next if column.compact.empty?
 
       winner = four_consecutive_chips?(column, 4)
@@ -46,26 +43,78 @@ class Board
     nil
   end
 
-  # def game_over?
-  #   pass
-  # end
+  def check_diagonal
+    diagonal_l_to_r = diagonal_l_to_r?
+    return diagonal_l_to_r if diagonal_l_to_r
+
+    diagonal_r_to_l = diagonal_r_to_l?
+    return diagonal_r_to_l if diagonal_r_to_l
+
+    nil
+  end
+
+  def game_over?
+    
+    check = check_horizontal
+    return check if check
+
+    check = check_vertical
+    return check if check
+
+    check = check_diagonal
+    return check if check
+
+    return 'tie' if grid_full?
+
+    nil
+  end
 
   private
 
   def four_consecutive_chips?(array, consecutive_chips)
     array.each_cons(consecutive_chips).any? do |sub_array|
-      sub_array.uniq.size == 1
+      sub_array.uniq.size == 1 && sub_array[0] != nil
     end
   end
 
   def valid_placement?(column)
-    column >= 0 && column < dimensions[0].size && !column_full?(column)
+    column >= 0 && column < grid[0].size && !column_full?(column)
   end
 
   def column_full?(column)
-    dimensions.each do |row|
+    grid.each do |row|
       return false if row[column].nil?
     end
     true
+  end
+
+  def diagonal_l_to_r?
+    0.upto(grid.size - 4).each do |i|
+      0.upto(grid[0].size - 4).each do |j|
+        diagonal = [grid[i][j], grid[i + 1][j + 1], grid[i + 2][j + 2], grid[i + 3][j + 3]]
+        next if diagonal.compact.empty?
+
+        winner = four_consecutive_chips?(diagonal, 4)
+        return diagonal[0] if winner
+      end
+    end
+    nil
+  end
+
+  def diagonal_r_to_l?
+    3.upto(grid.size - 1).each do |i|
+      0.upto(grid[0].size - 4).each do |j|
+        diagonal = [grid[i][j], grid[i - 1][j + 1], grid[i - 2][j + 2], grid[i - 3][j + 3]]
+        next if diagonal.compact.empty?
+
+        winner = four_consecutive_chips?(diagonal, 4)
+        return diagonal[0] if winner
+      end
+    end
+    nil
+  end
+
+  def grid_full?
+    grid.flatten.none?(&:nil?)
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,31 +6,31 @@ describe Board do
   describe '#initialize' do
     it 'creates a 6x7 game board' do
       board = Board.new
-      expect(board.dimensions.size).to eq(6)
-      expect(board.dimensions[0].size).to eq(7)
+      expect(board.grid.size).to eq(6)
+      expect(board.grid[0].size).to eq(7)
     end
 
     it 'creates a 7x6 game board' do
       board = Board.new(7, 6)
-      expect(board.dimensions.size).to eq(7)
-      expect(board.dimensions[0].size).to eq(6)
+      expect(board.grid.size).to eq(7)
+      expect(board.grid[0].size).to eq(6)
     end
 
     it 'creates an empty game board' do
       board = Board.new
-      expect(board.dimensions).to eq(Array.new(6) { Array.new(7) })
+      expect(board.grid).to eq(Array.new(6) { Array.new(7) })
     end
 
     it 'creates a game board with a minimum of 4 rows' do
       board = described_class.new(4, 6)
-      expect(board.dimensions.size).to eq(4)
-      expect(board.dimensions[0].size).to eq(6)
+      expect(board.grid.size).to eq(4)
+      expect(board.grid[0].size).to eq(6)
     end
 
     it 'creates a game board with a minimum of 4 columns' do
       board = described_class.new(6, 4)
-      expect(board.dimensions.size).to eq(6)
-      expect(board.dimensions[0].size).to eq(4)
+      expect(board.grid.size).to eq(6)
+      expect(board.grid[0].size).to eq(4)
     end
   end
 
@@ -40,24 +40,24 @@ describe Board do
 
     it 'place chip at the bottom cell in first column when entire column is unoccupied' do
       board_chip.place_chip(player, 0)
-      expect(board_chip.dimensions[5][0]).to eq('red')
+      expect(board_chip.grid[5][0]).to eq('red')
     end
 
     it 'place chip at the second cell in first column when first cell is occupied' do
       board_chip.place_chip(player, 0)
       board_chip.place_chip(player, 0)
-      expect(board_chip.dimensions[4][0]).to eq('red')
+      expect(board_chip.grid[4][0]).to eq('red')
     end
 
     it 'place chip at the third cell in third column when two first cells are occupied' do
       board_chip.place_chip(player, 2)
       board_chip.place_chip(player, 2)
       board_chip.place_chip(player, 2)
-      expect(board_chip.dimensions[3][2]).to eq('red')
+      expect(board_chip.grid[3][2]).to eq('red')
     end
 
     it 'raises an error if the column is full and cannot place chip' do
-      board_chip.dimensions = Array.new(6) { Array.new(7, player.color) }
+      board_chip.grid = Array.new(6) { Array.new(7, player.color) }
       expect { board_chip.place_chip(player, 0) }.to raise_error('Column is full')
     end
 
@@ -120,23 +120,108 @@ describe Board do
     end
   end
 
-  describe '#check_horizontal_left_right' do
-    it "return 'red' when player 1 has an horizontal top left to bottom right win" do
+  describe '#check_diagonal' do
+    it "return 'red' when player 1 has a diagonal top left to bottom right win" do
+      board = Board.new
+      board.grid =
+        [
+          [nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil],
+          ['red', nil, nil, nil, nil, nil, nil],
+          [nil, 'red', nil, nil, nil, nil, nil],
+          [nil, nil, 'red', nil, nil, nil, nil],
+          [nil, nil, nil, 'red', nil, nil, nil]
+        ]
+
+      expect(board.check_diagonal).to eq('red')
+    end
+
+    it "return 'red' when player 1 has an diagonal bottom left to top right win" do
+      board = Board.new
+      board.grid =
+        [
+          [nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, 'red', nil, nil, nil],
+          [nil, nil, 'red', nil, nil, nil, nil],
+          [nil, 'red', nil, nil, nil, nil, nil],
+          ['red', nil, nil, nil, nil, nil, nil]
+        ]
+
+      expect(board.check_diagonal).to eq('red')
+    end
+
+    it "return 'yellow' when player 2 has an diagonal top left to bottom right win" do
+      board = Board.new
+      board.grid =
+        [
+          [nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil],
+          ['yellow', nil, nil, nil, nil, nil, nil],
+          [nil, 'yellow', nil, nil, nil, nil, nil],
+          [nil, nil, 'yellow', nil, nil, nil, nil],
+          [nil, nil, nil, 'yellow', nil, nil, nil]
+        ]
+
+      expect(board.check_diagonal).to eq('yellow')
+    end
+
+    it 'return nil if four diagonal chips are not the same color' do
+      board = Board.new
+      board.grid =
+        [
+          [nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil],
+          ['yellow', nil, nil, nil, nil, nil, nil],
+          [nil, 'red', nil, nil, nil, nil, nil],
+          [nil, nil, 'yellow', nil, nil, nil, nil],
+          [nil, nil, nil, 'red', nil, nil, nil]
+        ]
+
+      expect(board.check_diagonal).to eq(nil)
     end
   end
 
   describe '#game_over?' do
+    subject(:board_game_over) { described_class.new }
+    let(:player1) { double('player1', color: 'red') }
+    let(:player2) { double('player2', color: 'yellow') }
 
-    xit 'returns nil if there is no win' do
+    it 'returns nil if there is no win' do
+      board_game_over.place_chip(player1, 0)
+      board_game_over.place_chip(player2, 1)
+      board_game_over.place_chip(player1, 2)
+      board_game_over.place_chip(player2, 3)
+      expect(board_game_over.game_over?).to eq(nil)
     end
 
-    xit 'player 1 has won the game' do
+    it 'player 1 has won the game' do
+      board_game_over.place_chip(player1, 0)
+      board_game_over.place_chip(player1, 1)
+      board_game_over.place_chip(player1, 2)
+      board_game_over.place_chip(player1, 3)
+      expect(board_game_over.game_over?).to eq('red')
     end
 
-    xit 'player 2 has won the game' do
+    it 'player 2 has won the game' do
+      board_game_over.place_chip(player2, 0)
+      board_game_over.place_chip(player2, 1)
+      board_game_over.place_chip(player2, 2)
+      board_game_over.place_chip(player2, 3)
+      expect(board_game_over.game_over?).to eq('yellow')
     end
 
-    xit 'game is tied' do
+    it 'game is tied' do
+      board_game_over.grid =
+      [
+        ['yellow', 'blue', 'luke', 'i', 'am', 'your', 'father'],
+        ['red', 'rorange', 'ja', 'bo', 'ti', 'ca', 'ba'],
+        ['yellow', 'red', 'dead', 'redemption', 'two', 'rock', 'star'],
+        ['red', 'red', 'man', 'i am', 'outta', 'words', 'to fill'],
+        ['yellow', 'yellow', 'pink', 'mango', 'fruit', 'loops', 'avocado'],
+        ['red', 'maybe red', 'not red', 'red', 'reddish', 'nil', 'pink']
+      ]
+      expect(board_game_over.game_over?).to eq('tie')
     end
   end
 end
